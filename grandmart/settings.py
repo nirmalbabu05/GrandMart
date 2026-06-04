88ee0ab51a8d8910d206aa2f4b7bccff664b56b8
NIRMAL BABU V M <159985369+nirmalbabu05@users.noreply.github.com>
2026-06-04 17:15:09 +0530

    Update settings.py

--- grandmart/settings.py
+++ grandmart/settings.py
@@ -81,11 +81,14 @@ WSGI_APPLICATION = 'grandmart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
+# DATABASE_URL செட்டிங்ஸ்
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 
