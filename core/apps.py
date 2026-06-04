49ea379af24a02b8964e2908f4c733647433e449
NIRMAL BABU V M <159985369+nirmalbabu05@users.noreply.github.com>
2026-06-04 16:48:15 +0530

    Enhance CoreConfig to create superuser on startup
    Added a ready method to create a superuser if it doesn't exist.

--- core/apps.py
+++ core/apps.py
@@ -1,5 +1,13 @@
 from django.apps import AppConfig
-
+import os
 
 class CoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        # சர்வர் ஸ்டார்ட் ஆகும்போது இதை ரன் பண்ணும்
+        from django.contrib.auth.models import User
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser('admin', 'admin@grandmart.com', 'admin123')
+            print("Superuser 'admin' created automatically!")
